b2e7646d3beb8fc4f6e47f4c25413cdfd5722cad
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-03-08 18:38:50 -0300

    WIP Solving Haskell bob exercise

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,17 @@
 module Bob (responseFor) where
 
+import Data.Char (isUpper, isAlpha, isSpace)
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor sentence
+  | isNothing  = "Fine. Be that way!"
+  | isShouting = "Whoa, chill out!"
+  | isForcefulQuestion = "Calm down, I know what I'm doing!"
+  | isQuestion = "Sure."
+  | otherwise  = "Whatever."
+  where areAllUpper   = all isUpper
+        isQuestion    = last sentence == '?'
+        isForcefulQuestion = areAllUpper (init sentence) && last sentence == '?'
+        isShouting    = saysSomething sentence && areAllUpper (filter isAlpha sentence)
+        saysSomething = any isAlpha
+        isNothing     = all isSpace sentence

8447ad65507510b2e99794b22e3b0fad164fdcdb
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-03-09 15:53:43 -0300

    Improve Haskell pangram solution

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -3,8 +3,7 @@ module Pangram (isPangram) where
 import Data.Char (isAlpha, toLower)
 
 isPangram :: String -> Bool
-isPangram "" = False
 isPangram text =
-  let alphaLowerText = filter isAlpha (map toLower text)
-      inAlphabet = [ x  `elem` alphaLowerText | x <- ['a'..'z'] ]
-  in all (== True) inAlphabet
+  let lowerText = map toLower text
+      alphabet = ['a'..'z']
+  in all (`elem` lowerText) alphabet

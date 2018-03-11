97c8ac83953f7907eb36650f31d56951e5003a73
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-03-10 21:18:05 -0300

    Solve Ruby gigasecond exercise

--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -4,27 +4,22 @@ require_relative 'gigasecond'
 # Common test data version: 1.0.0 61e7d70
 class GigasecondTest < Minitest::Test
   def test_date_only_specification_of_time
-    # skip
     assert_equal Time.utc(2043, 1, 1, 1, 46, 40), Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
   end
 
   def test_second_test_for_date_only_specification_of_time
-    skip
     assert_equal Time.utc(2009, 2, 19, 1, 46, 40), Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
   end
 
   def test_third_test_for_date_only_specification_of_time
-    skip
     assert_equal Time.utc(1991, 3, 27, 1, 46, 40), Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
   end
 
   def test_full_time_specified
-    skip
     assert_equal Time.utc(2046, 10, 2, 23, 46, 40), Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
   end
 
   def test_full_time_with_day_roll_over
-    skip
     assert_equal Time.utc(2046, 10, 3, 1, 46, 39), Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
   end
 
@@ -46,7 +41,6 @@ class GigasecondTest < Minitest::Test
   # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
 
   def test_bookkeeping
-    skip
     assert_equal 6, BookKeeping::VERSION
   end
 end

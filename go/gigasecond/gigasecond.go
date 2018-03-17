74d7164de9f0dcdca294586ca38f35724999cec7
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-03-17 16:30:01 -0300

    Remove stub comments from golang gigasecond

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,11 +1,5 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package gigasecond should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
 const gigasecond = time.Duration(1000000000) * time.Second

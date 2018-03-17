21852e2e9df5fb73680f77233ba86c62e2d9475f
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2018-03-17 16:28:35 -0300

    Improve golang gigasecond exercise

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -8,11 +8,9 @@ package gigasecond
 // import path for the time package from the standard library
 import "time"
 
-// AddGigasecond should have a comment documenting it.
+const gigasecond = time.Duration(1000000000) * time.Second
+
+// AddGigasecond receives a time then add 1 gigasecond to it
 func AddGigasecond(t time.Time) time.Time {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return t.Add(time.Duration(1000000000 * 1000 * 1000 * 1000))
+	return t.Add(gigasecond)
 }
